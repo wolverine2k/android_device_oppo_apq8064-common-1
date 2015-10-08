657b6113599e4a67617bedea5d6da20805a2aedb
wuxianlin <wuxianlinwxl@gmail.com>
2015-10-08 20:42:35 +0800

    lvm policy from find7 omnirom

--- patches/install.sh
+++ patches/install.sh
@@ -9,6 +9,18 @@ echo "Applying frameworks/base patches..."
 git apply $rootdirectory/device/oppo/apq8064-common/patches/frameworks/base/*.patch
 echo " "
 
+cd $rootdirectory
+cd external/sepolicy
+echo "Applying external/sepolicy patches..."
+git apply $rootdirectory/device/oppo/apq8064-common/patches/external/sepolicy/*.patch
+echo " "
+
+cd $rootdirectory
+cd system/core
+echo "Applying system/core patches..."
+git apply $rootdirectory/device/oppo/apq8064-common/patches/system/core/*.patch
+echo " "
+
 
 # -----------------------------------
 echo "Changing to build directory..."
--- patches/uninstall.sh
+++ patches/uninstall.sh
@@ -9,6 +9,18 @@ echo "Reverting frameworks/base patches..."
 git apply --reverse $rootdirectory/device/oppo/apq8064-common/patches/frameworks/base/*.patch
 echo " "
 
+cd $rootdirectory
+cd external/sepolicy
+echo "Reverting external/sepolicy patches..."
+git apply --reverse $rootdirectory/device/oppo/apq8064-common/patches/external/sepolicy/*.patch
+echo " "
+
+cd $rootdirectory
+cd system/core
+echo "Reverting system/core patches..."
+git apply --reverse $rootdirectory/device/oppo/apq8064-common/patches/system/core/*.patch
+echo " "
+
 
 # -----------------------------------
 echo "Changing to build directory..."
